f70c3e4860325f8a5cf0ab9d9d7f36e5b10c9fb4
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-07-06 09:20:42 -0500

    Creacion del proyecto, layout de color fragment, recursos

---
+++ app/src/main/java/unicauca/movil/holamundo/fragmentii/fragments/ColorFragment.java
@@ -0,0 +1,49 @@
+package unicauca.movil.holamundo.fragmentii.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import unicauca.movil.holamundo.fragmentii.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ColorFragment extends Fragment {
+
+    public static final int COLOR_AZUL=0;
+    public static final int COLOR_ROJO=1;
+    public static final int COLOR_AMARILLO=2;
+    public static final int COLOR_VERDE=3;
+    public static final int COLOR_VIOLETA=4;
+    public static final int COLOR_NARANJA=5;
+
+    int color;
+
+    public ColorFragment() {
+        // Required empty public constructor
+    }
+
+    public void init(int color){
+        this.color = color;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.fragment_color, container, false);
+
+        int colors[] = getActivity().getResources().getIntArray(R.array.colores);
+        String colorsN[] = getActivity().getResources().getStringArray(R.array.colores_n);
+
+
+
+        return v;
+
+    }
+
+
+}
